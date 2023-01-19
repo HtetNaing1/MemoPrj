fa8d12f931368b198e6dfada3c27ff65506c24ce
Htet Naing <htetnaing.chess@gmail.com>
2023-01-19 17:15:45 +0630

    Changed Database

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://Htet_Naing:hnaing123@memories.5d6xnmj.mongodb.net/?retryWrites=true&w=majority'
+const CONNECTION_URL = 'mongodb+srv://HtetNaing:hnaing123@memories.uocopdc.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
